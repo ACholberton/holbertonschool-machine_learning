df5cab07a096bedc5dd90243383c1876bb7400c8
Andres_CH <55113100+ACholberton@users.noreply.github.com>
2020-07-15 00:33:56 -0700

    Added files for task 1 and 2

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+"""This funciton calculates the shape of matrix"""
+import numpy as np
+
+
+def matrix_shape(matrix):
+    """Function to calculate the shape"""
+
+    matrix = np.matrix
+    return (matrix.shape)

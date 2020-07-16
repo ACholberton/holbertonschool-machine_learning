ea7b2e743a2f2b1e2730b054737bb08ccd54454d
Andres_CH <55113100+ACholberton@users.noreply.github.com>
2020-07-15 17:06:04 -0700

    Created new list for the result of the sum

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -3,8 +3,10 @@
 
 def add_arrays(arr1, arr2):
 
+    NL = []
     if len(arr1) == len(arr2):
         """Checks if both arrays have the same length"""
-        return [arr1[row] + arr2[row] for row in range(len(arr1))]
+        NL = [arr1[row] + arr2[row] for row in range(len(arr1))]
+        return NL
     else:
         return None

83444c8fb8cfdc9074e7f8176bb0445260d02526
ACholberton <1103@holbertonschool.com>
2020-07-18 20:26:18 +0000

    Fixed shebang, pep validation and indentation

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ This funciton performs a matrix multiplication """
 import numpy as np
 
 
-
 def np_matmul(mat1, mat2):
+    """ using matmul in the numpy library we can multiply matrices"""
     return np.matmul(mat1, mat2)

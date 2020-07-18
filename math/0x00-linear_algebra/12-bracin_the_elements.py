04cf81af263c39e7e010c3da691883e5462f77b7
ACholberton <1103@holbertonschool.com>
2020-07-18 20:20:09 +0000

    fixed shebang, indentation and pep8 validation

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3.5
-""" This funciton performs element-wise addition, substraction, multiplication, division """
+#!/usr/bin/env python3
+""" This funciton performs element-wise calculations """
+
 
 def np_elementwise(mat1, mat2):
+    """ This function can perform several mathematical calculations"""
     return (mat1 + mat2), (mat1 - mat2), (mat1 * mat2), (mat1 / mat2)

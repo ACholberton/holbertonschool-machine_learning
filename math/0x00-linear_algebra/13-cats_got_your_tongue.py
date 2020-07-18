782838e990bb88e08ed95719ac2d60a2b407284e
ACholberton <1103@holbertonschool.com>
2020-07-18 20:24:50 +0000

    Fixed shebang, indentation and pep8 validation

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Function that concatenates two matrices along a specific axis"""
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
+    """This funciton uses numpy to concatenate matrices along an axis"""
     return np.concatenate((mat1, mat2), axis=axis)

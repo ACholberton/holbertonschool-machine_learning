b831ffaba54f02580952cc8709f43360ee8c98e3
ACholberton <1103@holbertonschool.com>
2020-07-18 20:17:09 +0000

    fixed shebang, documentation and indentation

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3.5
+#!/usr/bin/env python3
 """ Function transpose a matrix """
 
+
 def np_transpose(matrix):
+    """using numpy.transpose we can transpose a matrix"""
     return (matrix.transpose())

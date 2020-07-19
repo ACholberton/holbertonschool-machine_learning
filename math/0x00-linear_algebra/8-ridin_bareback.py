72cdfdace60243bb8f854c6e0b5fd30c0dcf1e88
ACholberton <1103@holbertonschool.com>
2020-07-19 21:03:50 +0000

    Fixed shebang and indentation errors

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" This funciton performs matrix multiplication """
+
 
 def mat_mul(mat1, mat2):
+    """using nested loops we iterate through
+    both matrices in order to get their shapes and multiply"""
+
     new_matrix = []
 
-    if len(mat1) == len(mat2):
-        for i in range(len(mat1)):
-            for j in range(len(mat2[0])):
-                for k in range(len(mat2)):
-                    new_matrix[i][j] += mat1[i][j] * mat2[i][j]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                new_matrix[i][j] += mat1[i][j] * mat2[i][j]
 
-    for m in new_matrix:
-        return (m)
+    return new_matrix

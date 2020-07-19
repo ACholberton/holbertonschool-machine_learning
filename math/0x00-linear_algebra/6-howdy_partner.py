1d73274930e077721d87642b540d43ea45c1ff3a
ACholberton <1103@holbertonschool.com>
2020-07-19 20:57:07 +0000

    fixed shebang

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ This function will concatenate 2 arrays """
 
 
 def cat_arrays(arr1, arr2):
+    """ returns the addition of both arrays"""
     return arr1 + arr2

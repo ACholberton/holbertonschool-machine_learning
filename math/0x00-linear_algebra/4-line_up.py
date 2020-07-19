47b2c251b01933a61b995c370974b99c72a23da1
ACholberton <1103@holbertonschool.com>
2020-07-19 20:34:05 +0000

    made nested loop to iterate through both arrays

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This funciton adds 2 arrays """
+
 
 def add_arrays(arr1, arr2):
 
     NL = []
     if len(arr1) == len(arr2):
         """Checks if both arrays have the same length"""
-        NL = [arr1[row] + arr2[row] for row in range(len(arr1))]
-        return NL
-    else:
         return None
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            NL.append(arr1[i] + arr2[2])
+
+    return NL

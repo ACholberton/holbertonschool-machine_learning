d1ba369f2d598e9bb081fb764b7de9ed69a99895
ACholberton <1103@holbertonschool.com>
2020-07-23 17:54:18 +0000

    fixed typos and funciton names

--- math/0x01-plotting/3-two.py
+++ math/0x01-plotting/3-two.py
@@ -9,13 +9,12 @@ t2 = 1600
 y1 = np.exp((r / t1) * x)
 y2 = np.exp((r / t2) * x)
 
-plt.plot(x, y)
 plt.legend()
 plt.xlabel('Time (years)')
 plt.ylabel('Fraction Remaining')
 plt.title('Exponential Decay of Radioactive Elements')
-plt.xlimit[(0, 20000)]
-plt.ylimit[(0, 1)]
+plt.xlim(0, 20000)
+plt.ylim(0.0, 1.0)
 plt.plot(x, y1, 'r--', label='C-14')
 plt.plot(x, y2, 'green', label='Ra-226')
 plt.show()

ff66e9d1bc5e00a9a5ec469a135bb037e83f357e
ACholberton <1103@holbertonschool.com>
2020-07-27 00:35:48 +0000

    made files executables and fixed pycodestyle

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,16 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
 people = ['Farrah', 'Fred', 'Felicia']
-colors =  ['red', 'yellow', '#ff8000', '#ffe5b4']
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 for j in range(len(people)):
-        a = 0
-        for i in range(len(fruit)):
-            plt.bar(people[j], fruit[i][j], bottom=a, width=0.5, color=colors[i])
-            a += fruit[i][j]
-
+    a = 0
+    for i in range(len(fruit)):
+        plt.bar(people[j],
+                fruit[i][j],
+                bottom=a,
+                width=0.5,
+                color=colors[i])
+        a += fruit[i][j]
 plt.ylim(0, 80)
 plt.legend(('apples', 'bananas', 'oranges', 'peaches'))
 plt.title('Number of Fruit per Person')

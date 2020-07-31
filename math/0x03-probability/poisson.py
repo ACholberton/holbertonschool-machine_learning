7de22d5209373bb250f6bb7f34758d03d7d40598
ACholberton <1103@holbertonschool.com>
2020-07-31 00:31:26 +0000

    fixed indentation

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,12 +6,14 @@ class Poisson:
     """poisson class"""
     e = 2.7182818285
     pi = 3.1415926536
+
     def __init__(self, data=None, lambtha=1.):
         """Class init"""
         if data is None and isinstance(lambtha, (float, int)):
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = lambtha
+            else:
+                self.lambtha = lambtha
         else:
             if not isinstance(data, int):
                 raise TypeError("data must be a list")

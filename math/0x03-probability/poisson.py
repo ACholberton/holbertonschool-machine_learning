3eb5b2ef3ff8648bb222fdeca4f0f763f0b00504
ACholberton <1103@holbertonschool.com>
2020-07-31 00:36:57 +0000

    fixed error where a tpe error was raised to an int instead of a list

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,18 +4,20 @@
 
 class Poisson:
     """poisson class"""
-    e = 2.7182818285
-    pi = 3.1415926536
 
     def __init__(self, data=None, lambtha=1.):
         """Class init"""
+
+        self.e = 2.7182818285
+        self.pi = 3.1415926536
+
         if data is None and isinstance(lambtha, (float, int)):
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             else:
                 self.lambtha = lambtha
         else:
-            if not isinstance(data, int):
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")

23934bed470b9c2ed7139bf9eb72484324b8dbc9
ACholberton <1103@holbertonschool.com>
2020-08-01 00:52:17 +0000

    created exponential class

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Class exponential """
+
+
+class Exponential:
+    """ class init """
+    def __init__(self, data=None, lambtha=1.):
+
+        self.e = 2.7182818285
+        self.pi = 3.1415926536
+
+        if data is None and isinstance(lambtha, (float, int)):
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = len(data) / sum(data)

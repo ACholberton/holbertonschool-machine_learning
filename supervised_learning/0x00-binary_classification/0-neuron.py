e00d4b0ca62aeb2499bf94e554435b824b0725c5
ACholberton <1103@holbertonschool.com>
2020-08-05 22:25:30 +0000

    created a class Neuron that defines a single neuron performing binary classification

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Class neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, nx):
+        self.nx = nx
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0

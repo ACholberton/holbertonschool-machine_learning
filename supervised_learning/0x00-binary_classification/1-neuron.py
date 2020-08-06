3741d7de44917c56f345735e1f0363d3130c3339
ACholberton <1103@holbertonschool.com>
2020-08-06 01:35:11 +0000

    created class neuron based on 0-neuron

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 """ Class neuron"""
-
 import numpy as np
 
 
 class Neuron:
+    """class init"""
+
     def __init__(self, nx):
-        """class init"""
-        self.nx = nx
 
         if type(nx) is not int:
-            raise TypeError("nx must be an integer")
+            raise TypeError("nx must be a integer")
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError("nx must be positive")
 
-        self._W = np.random.normal(size=(1, nx))
-        self._b = 0
-        self._A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
-        @property
-        def W(self):
-            """Return weights"""
-            return self._W
-            
-        @property
-        def b(self):
-            """Returns bias"""
-            return self._b
+    @property
+    def W(self):
+        """Return weights"""
+        return self.__W
 
-        @property
-        def A(self):
-            """Return output"""
-            return self._A
+    @property
+    def b(self):
+        """return bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """return output"""
+        return self.__A

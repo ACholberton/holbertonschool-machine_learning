12df0243b687bd3414b6169f5b25bebab6e4144e
ACholberton <1103@holbertonschool.com>
2020-08-09 00:08:26 +0000

    created function that claculates the cost of the model using logistic regresion

---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Class neuron"""
+import numpy as np
+
+
+class Neuron:
+    """class init"""
+
+    def __init__(self, nx):
+
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Return weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """return bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """return output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """foward propagation"""
+        i = np.matmul(self.__W, X) + self.__b
+        self.__A = self.sigmoid_func(i)
+        return self.__A
+
+    def sigmoid_func(self, X):
+        """sigmoid function"""
+        return 1.0/(1.0 + np.exp(-X))
+
+    def cost(self, Y, A):
+        """Cost of Model using logistic regresion"""
+        return -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()

ec6327768c2b85e1374781e62455a0eb5b213108
ACholberton <1103@holbertonschool.com>
2020-08-12 04:52:14 +0000

    fixed errors

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,12 +6,14 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
+    """Y is a numpy array, 
+    and classes is the maximum number of classes found in Y"""
 
     if not isinstance(Y, np.ndarray) or len(Y) == 0:
         return None
     if not isinstance(classes, int) or classes < np.max(Y) + 1:
         return None
     A = np.zeros((classes, Y.shape[0]))
-    for cl, m in enumerate(Y):
-        A[m][cl] = 1
+    for i, m in enumerate(Y):
+        A[m][i] = 1
     return A
